9b8f76f7777117f41c72c5669d575219e14bc648
Chris Mutel <cmutel@gmail.com>
2024-05-12 05:28:25 -0400

    Skip simple estimation test on Windows
    Clock timer resolution is too close

--- tests/test_estimation.py
+++ tests/test_estimation.py
@@ -1,6 +1,14 @@
+import sys
+
+import pytest
+
 from bw_aggregation import AggregatedDatabase, Speedup
 
 
+@pytest.mark.skipif(
+    sys.platform.startswith("win"),
+    reason="Windows `time` function has too low resolution",
+)
 def test_speedup_estimate(background):
     speedup = AggregatedDatabase.estimate_speedup("a")
     assert isinstance(speedup, Speedup)
